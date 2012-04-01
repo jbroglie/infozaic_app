e2a94f94152bdfbed5094fd5e6f81ed61f11c94c
Justin Broglie <jbroglie@gmail.com>
2012-04-01 12:30:51 -0400

    first commit for infozaic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfozaicApp::Application.config.secret_token = 'c24ad0d048f2e00bdba2a2d96ddda3ec413cca095bdfc7bc0d2e8f9d991268dc31e7cde8a5fd96a3702a216cf0cc4e1c763a65cb0dca2a1301f8cdb154809958'
