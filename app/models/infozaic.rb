57cbbeca28cf19ddf907b10d6ff3b7a50319b603
Justin Broglie <jbroglie@gmail.com>
2012-04-05 03:27:24 -0400

    added password_digest and index on email, finish chapter 6

--- app/models/infozaic.rb
+++ app/models/infozaic.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: infozaics
+#
+#  id          :integer         not null, primary key
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
 class Infozaic < ActiveRecord::Base
   attr_accessible :description, :title
 end
--- spec/models/infozaic_spec.rb
+++ spec/models/infozaic_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: infozaics
+#
+#  id          :integer         not null, primary key
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Infozaic do
