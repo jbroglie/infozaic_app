a0524a6f36aacb5bd33a6ebc48f19e6c2d8a0db6
Justin Broglie <jbroglie@gmail.com>
2012-04-25 02:22:01 -0400

    remove static infobits

--- app/models/infozaic.rb
+++ app/models/infozaic.rb
@@ -15,4 +15,9 @@ class Infozaic < ActiveRecord::Base
 
   has_many :infobits, :dependent => :destroy
 
+  validates :title, presence:   true,
+  					length: { maximum: 255 }
+  validates :description, 	presence:   true,
+  							length: { maximum: 500 }
+
 end
