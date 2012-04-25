ec691a5cc993d0db4b13ef8ed4718148804bc38d
Justin Broglie <jbroglie@gmail.com>
2012-04-25 00:23:43 -0400

    reponsive layouts now somewhat working

---
+++ db/migrate/20120425015326_add_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :facebook, :string
+    add_column :users, :linkedin, :string
+    add_column :users, :twitter, :string
+    add_column :users, :bio, :text
+  end
+end
