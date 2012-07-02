2f1e7bf4190904fad2b1d4041dca089b3c87ae45
Justin Broglie <jbroglie@gmail.com>
2012-07-02 18:32:45 -0400

    fixed problem on embeds - trial 2, justin is being annoying hehe

--- app/controllers/infozaics_controller.rb
+++ app/controllers/infozaics_controller.rb
@@ -45,7 +45,7 @@ class InfozaicsController < ApplicationController
 
   def embed
     @infozaic = Infozaic.find(params[:id])
-    @infobits = Infobit.find(:all, :order => "created_at DESC", :limit => 10)
+    @infobits = Infobit.find(:all, :order => "created_at DESC", :limit => 15)
     @youtube_infobit = @infozaic.infobits.new
     @photo_infobit = @infozaic.infobits.new
     @quote_infobit = @infozaic.infobits.new
